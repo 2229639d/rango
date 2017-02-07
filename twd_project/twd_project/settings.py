f5b1c047bbbd52d8d3aab6b8c9206951df19ee69
Ivo Domingos <2229639d@student.gla.ac.uk>
2017-02-07 15:38:05 +0000

    Chapter 4: DONE!

--- twd_project/twd_project/settings.py
+++ twd_project/twd_project/settings.py
@@ -52,10 +52,18 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'twd_project.urls'
 
+TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
+
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
+
+MEDIA_DIR = os.path.join(BASE_DIR, 'media')
+
+STATICFILES_DIRS = [STATIC_DIR,]
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIR,],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -63,6 +71,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+				'django.template.context_processors.media'
             ],
         },
     },
@@ -119,3 +128,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_ROOT = MEDIA_DIR
+MEDIA_URL = '/media/'
